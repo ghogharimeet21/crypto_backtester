72e0ea57fdd09a2a7072923354bafd578aaa270b
Meet <161622011+ghogharimeet21@users.noreply.github.com>
2026-04-20 19:07:36 +0530

    added exception handlings

--- engine/evaluator/sample_strategy/__init__.py
+++ engine/evaluator/sample_strategy/__init__.py
@@ -10,9 +10,5 @@ from data.storage import meta_data
 def excecute(strategy: SampleStrategy):
 
 
-    quote = meta_data.get_quote(strategy.symbol, strategy.entry_date, strategy.entry_time)
-    
-
-    print(quote)
     
     ...
